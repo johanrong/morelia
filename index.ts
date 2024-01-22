5c136b8848d38776a93393881037228635081fdd
Johan Rong <59508244+johainworks@users.noreply.github.com>
2024-01-22 19:31:17 +0100

    some first stuff

---
+++ index.ts
@@ -0,0 +1,27 @@
+enum InputToken {
+    Equals = "Equals",
+    Plus = "Plus",
+    Minus = "Minus",
+    Asterisk = "Asterisk",
+    Slash = "Slash",
+    LParen = "LParen",
+    RParen = "RParen",
+    LCurly = "LCurly",
+    RCurly = "RCurly",
+
+    Int = "Int",
+    Float = "Float",
+    String = "String",
+    Ident = "Ident",
+}
+
+enum OutputToken {}
+
+function Tokenize(input: String) {
+    let output: String[] = []
+}
+
+function Parse() {}
+function Generate() {}
+
+// Make the input things and stuff
