fcfe42ba7c8f994e0fe56237c4683142cc665e09
Johan Rong <59508244+johainworks@users.noreply.github.com>
2024-01-24 18:58:32 +0100

    adding keywords and such && fixed parsing function calls and declaration and variable assignment and usage (kind of)

--- index.ts
+++ index.ts
@@ -108,7 +108,7 @@ function Tokenize(input: string) {
             if (result.includes(".")) output.push(BaseToken.Float + ":" + result)
             else output.push(BaseToken.Integer + ":" + result)
         } else if (c === '"') {
-            let result = ""
+            let result = '"'
             let j = i + 1
             
             for (j; j < chars.length; j++) {
@@ -120,7 +120,7 @@ function Tokenize(input: string) {
             }
             
             i += j - i
-            output.push(BaseToken.String + ":" + result)
+            output.push(BaseToken.String + ":" + result + '"')
         } else if (/^[a-zA-Z_]+$/.test(c)) {
             let result = ""
             let j = i
@@ -135,19 +135,19 @@ function Tokenize(input: string) {
 
             i += j - i - 1
             output.push(BaseToken.Identifier + ":" + result)
-        } else if (c === "^") {
+        } else if (c === "@") {
             let result = ""
-            let j = i + 2
+            let j = i + 1
 
             for (j; j < chars.length; j++) {
-                if (chars[j] !== '"') {
+                if (/^[a-zA-Z0-9_]+$/.test(chars[j])) {
                     result += chars[j]
                 } else {
                     break
                 }
             }
 
-            i += j - i
+            i += j - i - 1
             output.push(BaseToken.Path + ":" + result)
         } else if (c === " ") { // ignore
             console.info("ignoring whitespace")
@@ -209,80 +209,119 @@ function Parse(toks: string[]) {
     
     for (let i = 0; i < toks.length; i++) {
         const t = toks[i]
-        
-        if (t.includes(BaseToken.Identifier)) { // this has to be redone in someway because it thought it through and i dont want to write it in a comment
-            let isVariableUsage = false
-            let isVariableAssignment = false
-            let isFunctionCall = false
-            let isFunctionDeclaration = false
-            
-            let hasArgs = false
-            
-            let openParenIndex
-            let closeParenIndex
-            let openCurlyIndex
-            let closeCurlyIndex
-            
-            if (toks[i + 1] === BaseToken.Equals) {
-                isVariableAssignment = true
-            } if (toks[i + 1] === BaseToken.OpenParen) {
-                // the identifier is either a function call or declaration
-                
-                openParenIndex = i + 1
-                if (toks[i + 2] !== BaseToken.CloseParen)
-                    hasArgs = true
-                
-                for (let j = i + 1; j < toks.length; j++) {
-                    if (toks[j] === BaseToken.CloseParen)
-                        closeParenIndex = j
-                    if (toks[j] === BaseToken.OpenCurly)
-                        openCurlyIndex = j
-                    if (toks[j] === BaseToken.CloseCurly)
-                        closeCurlyIndex = j
-                }
-                
-                if (openCurlyIndex) {
-                    isFunctionDeclaration = true
+
+        if (TokenPart(t)[0] === BaseToken.Identifier) {
+            // check if identifier matches a keyword
+
+            if (TokenPart(t)[1] === "import") {
+                if (toks[i + 1] && TokenPart(toks[i + 1])[0] === BaseToken.Path) {
+                    output.push(IRToken.Import + ";" + toks[i + 1])
                 } else {
-                    isFunctionCall = true
+                    console.error("morelia: import requires valid path")
+                    process.exit(1)
                 }
-            } else {
-                // the identifier is a variable usage
-                isVariableUsage = true
-            }
-            
-            if (isFunctionDeclaration) {
-                // parse args
-                output.push(IRToken.Declare + ":" + TokenPart(t)[1])
-            } else if (isFunctionCall) {
-                if (hasArgs) {
-                    // @ts-ignore
-                    const [args, argAmount] = parseArgs(openParenIndex, closeParenIndex, toks)
-                    output.push(IRToken.Function + ":" + TokenPart(t)[1])
-                    output.push(IRToken.Args + ":" + args)
-                    i += 2 // skip identifier and openParen
-                    i += (Number(argAmount))
-                    i++ // skip close paren
+
+                i++ // eat the path
+            } else if (TokenPart(t)[1] === "from") {
+                if (toks[i + 1] && TokenPart(toks[i + 1])[0] === BaseToken.Path) {
+                    if (toks[i + 2] && TokenPart(toks[i + 2])[0] === BaseToken.Identifier && TokenPart(toks[i + 2])[1] === "import") {
+                        if (toks[i + 3] && TokenPart(toks[i + 3])[0] === BaseToken.Path) {
+                            output.push(IRToken.From + ";" + toks[i + 1] + ":" + IRToken.Import + ";" + toks[i + 3])
+                        } else {
+                            console.log("morelia: import requires valid path")
+                        }
+                    } else {
+                        console.error("morelia: from requires import")
+                        process.exit(1)
+                    }
                 } else {
-                    i += 2 // skip identifier and openParen
-                    output.push(IRToken.Function + ":" + TokenPart(t)[1])
-                    i++ // skip closeParen
+                    console.error("morelia: from requires valid path")
+                    process.exit(1)
                 }
-            } else if (isVariableAssignment) {
-                output.push(IRToken.Assign + ":" + TokenPart(t)[1])
-                i++
-            } else if (isVariableUsage) {
-                output.push(IRToken.Variable + ":" + TokenPart(t)[1])
+                
+                i += 3 // eat the path, import and import path
+            } else if (TokenPart(t)[1] === "while") {
+            } else if (TokenPart(t)[1] === "for") {
+            } else if (TokenPart(t)[1] === "if") {
             } else {
-                console.error("morelia: something horrible has happened")
+                let isVariableUsage = false
+                let isVariableAssignment = false
+                let isFunctionCall = false
+                let isFunctionDeclaration = false
+
+                let hasArgs = false
+
+                let openParenIndex
+                let closeParenIndex
+                let openCurlyIndex
+                let closeCurlyIndex
+
+                if (toks[i + 1] === BaseToken.Equals) {
+                    isVariableAssignment = true
+                }
+                if (toks[i + 1] === BaseToken.OpenParen) {
+                    // the identifier is either a function call or declaration
+
+                    openParenIndex = i + 1
+                    if (toks[i + 2] !== BaseToken.CloseParen)
+                        hasArgs = true
+
+                    for (let j = i + 1; j < toks.length; j++) {
+                        if (!closeParenIndex && toks[j] === BaseToken.CloseParen)
+                            closeParenIndex = j
+                        if (!openCurlyIndex && toks[j] === BaseToken.OpenCurly)
+                            openCurlyIndex = j
+                        if (!closeCurlyIndex && toks[j] === BaseToken.CloseCurly)
+                            closeCurlyIndex = j
+                    }
+                    
+                    if (openParenIndex && !closeParenIndex) {
+                        console.error("morelia: found opening parenthesis, but no closing parenthesis")
+                        process.exit(1)
+                    }
+
+                    if (openCurlyIndex) {
+                        isFunctionDeclaration = true
+                    } else {
+                        isFunctionCall = true
+                    }
+                } else {
+                    // the identifier is a variable usage
+                    isVariableUsage = true
+                }
+
+                if (isFunctionDeclaration) {
+                    // parse args
+                    output.push(IRToken.Declare + ";" + TokenPart(t)[1])
+                } else if (isFunctionCall) {
+                    if (hasArgs) {
+                        // @ts-ignore
+                        const [args, argAmount] = parseArgs(openParenIndex, closeParenIndex, toks)
+                        output.push(IRToken.Function + ";" + TokenPart(t)[1])
+                        output.push(IRToken.Args + ";" + args)
+                        i += 2 // skip identifier and openParen
+                        i += (Number(argAmount))
+                        i++ // skip close paren
+                    } else {
+                        i += 2 // skip identifier and openParen
+                        output.push(IRToken.Function + ";" + TokenPart(t)[1])
+                        i++ // skip closeParen
+                    }
+                } else if (isVariableAssignment) {
+                    output.push(IRToken.Assign + ";" + TokenPart(t)[1])
+                    i++
+                } else if (isVariableUsage) {
+                    output.push(IRToken.Variable + ";" + TokenPart(t)[1])
+                } else {
+                    console.error("morelia: something horrible has happened")
+                }
+
             }
-            
-            // skip indices
-        } else if (t.includes(BaseToken.Integer)) {
+        } else if (TokenPart(t)[0] === BaseToken.Integer) {
             // TODO: check if there is something wrong
             
-            output.push(IRToken.Integer + ":" + TokenPart(t)[1])
-        } else if (t === BaseToken.Newline) {
+            output.push(IRToken.Integer + ";" + TokenPart(t)[1])
+        } else if (TokenPart(t)[0] === BaseToken.Newline) {
             output.push(IRToken.Newline)
         } else {
             console.log("unparsed token found in parser: " + t)
@@ -296,7 +335,7 @@ function Parse(toks: string[]) {
 function Generate(ir: string[]) {}
 
 // Make the input things and stuff
-const source: string = 'x = 25\nprint(1, 2, 3, 4)\nprint(x)'
+const source: string = 'from @math import @pi'
 const tokens= Tokenize(source)
 console.log(tokens)
 const ir = Parse(tokens)
