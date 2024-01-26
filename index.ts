8424c5f65cc8d1245387f710029b22a0ed09f5fa
Johan Rong <59508244+johainworks@users.noreply.github.com>
2024-01-26 23:48:32 +0100

    wooho

--- index.ts
+++ index.ts
@@ -27,36 +27,21 @@ enum BaseToken {
     Identifier = "Identifier",
 }
 
+// TODO: rewrite the tokens that come out of the parser to almost python code
+
 // Intermediate Representation tokens for Parser
 enum IRToken {
-    Import = "Import",
-    From = "From",
-    While = "While",
-    For = "For",
-    If = "If",
-    Return = "Return",
-    
-    Declare = "Declare",
-    Assign = "Assign",
-    Args = "Args",
-    End = "End",
-    
-    Plus = "Plus",
-    Minus = "Minus",
-    Asterisk = "Asterisk",
-    Slash = "Slash",
-    
-    Function = "Function",
-    Variable = "Variable",
-    Integer = "Integer",
-    String = "String",
-    Float = "Float",
-    Path = "Path",
     Newline = "Newline",
+    Outdent = "Outdent",
+    Indent = "Indent",
 }
 
 function TokenPart(string: string) {
     return string.split(":")
+}
+
+function indent(indents: number) {
+    return " ".repeat(indents)
 }
 
 // Tokenizer/Lexer
@@ -121,12 +106,13 @@ function Tokenize(input: string) {
                 if (chars[j] !== '"') {
                     result += chars[j]
                 } else {
+                    result += '"'
                     break
                 }
             }
             
             i += j - i
-            output.push(BaseToken.String + ":" + result + '"')
+            output.push(BaseToken.String + ":" + result)
         } else if (/^[a-zA-Z_]+$/.test(c)) {
             let result = ""
             let j = i
@@ -184,44 +170,31 @@ function Tokenize(input: string) {
     return output
 }
 
-function parseArgs(openParenIndex: number,  closeParenIndex: number, toks: string[]) {
-    let args: string[] = []
-    let argAmount = 0
-    console.log(openParenIndex)
-    console.log(closeParenIndex)
-    
-    for (let i = openParenIndex + 1; i < closeParenIndex; i++) {
-        if (toks[i] === BaseToken.Comma) {
-            args.push(",")
-        } else if (toks[i].includes(BaseToken.Identifier)) {
-            if (toks[i + 1] === BaseToken.OpenParen) {
-                args.push(IRToken.Function + ":" + TokenPart(toks[i])[1])
-            } else {
-                args.push(IRToken.Variable + ":" + TokenPart(toks[i])[1])
-            }
-        } else {
-            args.push(toks[i])
-        }
-        
-        argAmount++
-    }
-    
-    return [args.join(""),  argAmount]
-}
-
 // Parser
 function Parse(toks: string[]) {
     let output: string[] = []
     
+    let openCurly = 0
+    
     for (let i = 0; i < toks.length; i++) {
         const t = toks[i]
 
-        if (TokenPart(t)[0] === BaseToken.Identifier) {
+        if (TokenPart(t)[0] === BaseToken.Equals) {
+            if (toks[i + 1] && TokenPart(toks[i + 1])[0] == BaseToken.Equals) {
+                // found double equals
+                output.push("==")
+            } else {
+                output.push("=")
+            }
+        } else if (TokenPart(t)[0] === BaseToken.Comma) {
+            output.push(",")
+        } else if (TokenPart(t)[0] === BaseToken.Identifier) {
             // check if identifier matches a keyword
 
             if (TokenPart(t)[1] === "import") {
                 if (toks[i + 1] && TokenPart(toks[i + 1])[0] === BaseToken.Path) {
-                    output.push(IRToken.Import + ";" + toks[i + 1])
+                    //output.push(IRToken.Import + ";" + toks[i + 1])
+                    output.push(`import ${TokenPart(toks[i + 1])[1]}`)
                 } else {
                     console.error("morelia: import requires valid path")
                     process.exit(1)
@@ -232,7 +205,8 @@ function Parse(toks: string[]) {
                 if (toks[i + 1] && TokenPart(toks[i + 1])[0] === BaseToken.Path) {
                     if (toks[i + 2] && TokenPart(toks[i + 2])[0] === BaseToken.Identifier && TokenPart(toks[i + 2])[1] === "import") {
                         if (toks[i + 3] && TokenPart(toks[i + 3])[0] === BaseToken.Path) {
-                            output.push(IRToken.From + ";" + toks[i + 1] + ":" + IRToken.Import + ";" + toks[i + 3])
+                            //output.push(IRToken.From + ";" + toks[i + 1] + ":" + IRToken.Import + ";" + toks[i + 3])
+                            output.push(`from ${TokenPart(toks[i + 1])[1]} import ${TokenPart(toks[i + 3])[1]}`)
                         } else {
                             console.log("morelia: import requires valid path")
                         }
@@ -250,106 +224,77 @@ function Parse(toks: string[]) {
             } else if (TokenPart(t)[1] === "for") {
             } else if (TokenPart(t)[1] === "if") {
             } else if (TokenPart(t)[1] === "return") {
-                output.push(IRToken.Return)
+                //output.push(IRToken.Return)
+                output.push("return ")
             } else {
-                let isVariableUsage = false
-                let isVariableAssignment = false
-                let isFunctionCall = false
-                let isFunctionDeclaration = false
-
-                let hasArgs = false
-
-                let openParenIndex
-                let closeParenIndex
-                let openCurlyIndex
-                let closeCurlyIndex
-
-                if (toks[i + 1] === BaseToken.Equals) {
-                    isVariableAssignment = true
+                let j = i + 1
+                let foundOpenCurly = false
+                for (j; j < toks.length; j++) {
+                    if (TokenPart(toks[j])[0] === BaseToken.OpenCurly) {
+                        if (openCurly !== j) {
+                            foundOpenCurly = true
+                            openCurly = j
+                            break
+                        }
+                    }
                 }
-                if (toks[i + 1] === BaseToken.OpenParen) {
-                    // the identifier is either a function call or declaration
-
-                    openParenIndex = i + 1
-                    if (toks[i + 2] !== BaseToken.CloseParen)
-                        hasArgs = true
-
-                    for (let j = i + 1; j < toks.length; j++) {
-                        if (!closeParenIndex && toks[j] === BaseToken.CloseParen)
-                            closeParenIndex = j
-                        if (!openCurlyIndex && toks[j] === BaseToken.OpenCurly)
-                            openCurlyIndex = j
-                        if (!closeCurlyIndex && toks[j] === BaseToken.CloseCurly)
-                            closeCurlyIndex = j
-                    }
-
-                    if (openParenIndex && !closeParenIndex) {
-                        console.error("morelia: found opening parenthesis, but no closing parenthesis")
-                        process.exit(1)
-                    }
-
-                    if (openCurlyIndex) {
-                        isFunctionDeclaration = true
-                    } else {
-                        isFunctionCall = true
-                    }
+                
+                if (foundOpenCurly) {
+                    output.push(`def ${TokenPart(t)[1]}`)
                 } else {
-                    // the identifier is a variable usage
-                    isVariableUsage = true
+                    output.push(TokenPart(t)[1])
                 }
-
-                if (isFunctionDeclaration) {
-                    // parse args
-                    if (hasArgs) {
-                        // @ts-ignore
-                        const [args, argAmount] = parseArgs(openParenIndex, closeParenIndex, toks)
-                        output.push(IRToken.Declare + ";" + TokenPart(t)[1] + ":" + IRToken.Args + ";" + args)
-                        i += 2 // skip identifier and openParen
-                        i += (Number(argAmount))
-                        i++ // skip openCurly
-                    } else {
-                        i += 2 // skip identifier and openParen
-                        output.push(IRToken.Function + ";" + TokenPart(t)[1])
-                    }
-                } else if (isFunctionCall) {
-                    if (hasArgs) {
-                        // @ts-ignore
-                        const [args, argAmount] = parseArgs(openParenIndex, closeParenIndex, toks)
-                        output.push(IRToken.Function + ";" + TokenPart(t)[1] + ":" + IRToken.Args + ";" + args)
-                        i += 2 // skip identifier and openParen
-                        i += (Number(argAmount))
-                    } else {
-                        i += 2 // skip identifier and openParen
-                        output.push(IRToken.Function + ";" + TokenPart(t)[1])
-                    }
-                } else if (isVariableAssignment) {
-                    output.push(IRToken.Assign + ";" + TokenPart(t)[1])
-                    i++
-                } else if (isVariableUsage) {
-                    output.push(IRToken.Variable + ";" + TokenPart(t)[1])
-                } else {
-                    console.error("morelia: something horrible has happened")
-                }
-
             }
-        } else if (TokenPart(t)[0] === BaseToken.CloseCurly) {
-            output.push(IRToken.End)
         } else if (TokenPart(t)[0] === BaseToken.Plus) {
-            output.push(IRToken.Plus)
+            output.push("+")
         } else if (TokenPart(t)[0] === BaseToken.Minus) {
-            output.push(IRToken.Minus)
+            output.push("-")
         } else if (TokenPart(t)[0] === BaseToken.Asterisk) {
-            output.push(IRToken.Asterisk)
+            output.push("*")
         } else if (TokenPart(t)[0] === BaseToken.Slash) {
-            output.push(IRToken.Slash)
+            output.push("/")
         } else if (TokenPart(t)[0] === BaseToken.Integer) {
-            // TODO: check if there is something wrong
+            output.push(TokenPart(t)[1])
+        } else if (TokenPart(t)[0] === BaseToken.Float) {
+            if (!TokenPart(t)[1].includes(".")) {
+                console.error("morelia: float does not include a period. (should not be possible)")
+                process.exit(1)
+            }
             
-            output.push(IRToken.Integer + ":" + TokenPart(t)[1])
+            if (TokenPart(t)[1].split(".").length - 1 !== 1) {
+                console.error("morelia: float contains multiple periods.")
+                process.exit(1)
+            }
+            
+            output.push(TokenPart(t)[1])
+        } else if (TokenPart(t)[0] === BaseToken.String) {
+            if (!TokenPart(t)[1].startsWith("\"")) {
+                console.error("morelia: string does not start with a quotation mark")
+                process.exit(1)
+            }
+
+            if (!TokenPart(t)[1].endsWith("\"")) {
+                console.error("morelia: string does not end with a quotation mark")
+                process.exit(1)
+            }
+
+            output.push(TokenPart(t)[1])
+        } else if (TokenPart(t)[0] === BaseToken.Path) {
+            output.push(TokenPart(t)[1])
+        } else if (TokenPart(t)[0] === BaseToken.OpenParen) {
+            output.push("(")
+        } else if (TokenPart(t)[0] === BaseToken.CloseParen) {
+            output.push(")")
+        } else if (TokenPart(t)[0] === BaseToken.OpenCurly) {
+            output.push(IRToken.Indent)
+        } else if (TokenPart(t)[0] === BaseToken.CloseCurly) {
+            output.push(IRToken.Outdent)
         } else if (TokenPart(t)[0] === BaseToken.Newline) {
-            output.push(IRToken.Newline)
+            if (TokenPart(toks[i - 1])[0] !== BaseToken.Newline) { // so there wont be to many newlines
+                output.push(IRToken.Newline)
+            }
         } else {
-            console.log("unparsed token found in parser: " + t)
+            console.log("morelia: unparsed token found in parser: " + t)
         }
     }
     
@@ -357,7 +302,33 @@ function Parse(toks: string[]) {
 }
 
 // Code generator
-function Generate(ir: string[]) {}
+function Generate(ir: string[]) {
+    let output = ""
+    let indents = 0
+    
+    for (let i = 0; i < ir.length; i++) {
+        let t = ir[i]
+        
+        if (t === IRToken.Newline) {
+            output += "\n"
+            output += indent(indents)
+        } else if (t === IRToken.Indent) {
+            indents += 4
+            output += ":"
+        } else if (t === IRToken.Outdent) {
+            if (indents - 4 >= 0) {
+                indents -= 4
+            } else {
+                console.error("morelia: indent count became negative (too many closing curly braces)")
+                process.exit(1)
+            }
+        } else {
+            output += t
+        }
+    }
+    
+    return output
+}
 
 // Make the input things and stuff
 //const source: string = 'from @math import @pi'
@@ -366,7 +337,5 @@ const tokens= Tokenize(await source)
 console.log(tokens)
 const ir = Parse(tokens)
 console.log(ir)
-//const output = Generate(ir)
-
-// write output to file
-//console.log(output)
+const output = Generate(ir)
+console.log(output)
